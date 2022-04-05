4763b4eb477a85359dffe73bf3296fbc6c47e5bd
mars908 <78188237+mars908@users.noreply.github.com>
2022-04-05 00:14:31 -0400

    about page done

--- app/app.js
+++ app/app.js
@@ -28,13 +28,26 @@ var homeContent = `<div class="hero"></div>
 var aboutContent = `<div class="hero"></div>
 
 <div class="content">
-                <h1>ABOUT</h1>
-                <div class="products">
-                    <div class="product">
+    <h1>ABOUT</h1>
+    <div class="products">
+        <div class="product">
+            <div class="about-images">
+                <div class="about-image1"></div>
+                <h3>Origin of the blue dragon name</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam et magnam architecto quas saepe voluptates maxime aliquid provident ratione beatae! Beatae fuga nostrum harum rerum ex eaque optio praesentium obcaecati pariatur, vitae consequuntur esse veritatis, totam ad? Perspiciatis, unde sapiente. Necessitatibus recusandae repudiandae ipsam cumque aliquam fuga! Hic, velit. Atque ipsum libero consequatur est eos voluptatem saepe, eveniet enim ut quod animi maiores inventore nihil laudantium laboriosam illo sapiente dicta sequi ratione molestias facilis mollitia? Ipsa architecto magni neque itaque dolorem quasi quidem nostrum quod expedita deserunt, cumque voluptatibus fugit. Voluptas excepturi quaerat facilis veniam praesentium necessitatibus dolores sed suscipit magni velit alias, illum magnam sequi temporibus eligendi minima quos, reiciendis pariatur? Assumenda aperiam libero molestias. Ducimus excepturi nisi nihil nulla.</p>
+                <div class="about-image2"></div>
+                <h3>Meet the owners</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor dignissimos neque necessitatibus voluptate nemo corporis? Dolorum perferendis, illo aperiam excepturi tempore veniam hic molestiae et ducimus veritatis sunt quo temporibus. Soluta ducimus fuga necessitatibus aliquid illum eaque numquam esse eos, nostrum unde. Voluptatum placeat hic, modi magnam sunt tempora totam error saepe eaque quia in eligendi dolorem iure incidunt autem dicta. Animi fugiat quis incidunt culpa natus veritatis praesentium voluptate minima amet! Mollitia, recusandae! Nostrum, debitis provident.</p>
+                <div class="about-image3"></div>
+                <h3>Humble beginnings</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quia tempore asperiores natus libero aperiam illo? Eligendi neque nisi harum sed, voluptate quibusdam at nulla architecto cumque aut nihil nobis, non magni vel eum veniam mollitia incidunt id sunt eveniet officiis necessitatibus dicta quisquam? Voluptates mollitia accusantium alias sunt? Sint incidunt optio asperiores sit, dolorem iure rerum reiciendis dolore iusto esse dignissimos sequi temporibus, quis distinctio placeat. Accusantium necessitatibus rem tenetur veritatis temporibus vel atque reprehenderit porro excepturi placeat nisi ab, maiores, sed sunt ducimus a dignissimos autem aliquid corporis quas nostrum, itaque saepe tempore. Eos aperiam qui ipsum placeat autem voluptatum sit quibusdam, voluptates cum, deserunt, minima illo dolore itaque quis harum voluptatibus! Quasi doloribus non, accusamus quidem perferendis reprehenderit mollitia consectetur!</p>
+                <div class="about-image4"></div>
+                <h3>Come visit us!</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cum doloremque dolore non? Ipsam quam harum dicta veritatis voluptates, sequi eaque facilis eveniet quia, exercitationem quae cupiditate nihil facere impedit soluta quo quisquam quos minima. Quaerat repellendus sequi at reiciendis a ad amet, nihil voluptatibus exercitationem repellat sapiente magnam nesciunt incidunt suscipit! Quidem, fuga enim in illo aliquam molestiae esse tempora mollitia accusantium, necessitatibus laborum, ad aperiam!</p>
+            </div>
                         <div class="prod-image"></div>
                         <div class="prod-desc">
-                            <h4>BIG CITY</h4>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dicta officia, reprehenderit sapiente quo numquam neque eaque ducimus accusamus inventore.</p>
+                            
                         </div>
                     </div>
                 </div>
